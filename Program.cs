66c647ea11d8160e42088f5baa5f656f4583aeba
Derek Chafin <infomaniac50@gmail.com>
2018-10-28 03:40:43 -0500

    Change Console.Write to Console.WriteLine.

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace ColorTemp
         {
             Color color = ColorTemperature.GetColorFromTemperature(opts.Temperature);
 
-            Console.Write("RGB: {0:F2}, {1:F2}, {2:F2}", color.R, color.G, color.B);
+            Console.WriteLine("RGB: {0:F2}, {1:F2}, {2:F2}", color.R, color.G, color.B);
             
             return 0;
         }
